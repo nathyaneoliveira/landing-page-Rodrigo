a0fe7145175467742a54b2251eaa83782a35e910
Nathyane Oliveira <nathyaneoliveira@hotmail.com>
2026-02-17 18:36:29 -0300

    landing page Rodrigo

--- js/script.js
+++ js/script.js
@@ -24,8 +24,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
         window.open(link, "_blank");
 
-        form.reset();
+        const toggle = document.getElementById('menu-toggle');
+        const menu = document.getElementById('menu');
+
+        toggle.addEventListener('click', () => {
+            menu.classList.toggle('active');
     });
+
+    });
+    
 
 });
 
