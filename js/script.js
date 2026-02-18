8e168dee1eb06438288586ff6480ac3af538e65d
Nathyane Oliveira <nathyaneoliveira@hotmail.com>
2026-02-18 00:25:35 -0300

    landing page Rodrigo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const NUMERO_WHATSAPP = "5583998565071"; // DDI + DDD + número
+    const NUMERO_WHATSAPP = "5583988731369"; // DDI + DDD + número
+//5583998565071
 
     const form = document.getElementById("formContato");
     if (!form) return;
@@ -8,27 +9,25 @@ document.addEventListener("DOMContentLoaded", function () {
     form.addEventListener("submit", function (e) {
         e.preventDefault();
 
-        const nome = document.getElementById("nome").value.trim();
-        const email = document.getElementById("email").value.trim();
-        const area = document.getElementById("area").value;
-        const mensagem = document.getElementById("mensagem").value.trim();
+        const nome = document.getElementById("nome")?.value.trim() || "";
+        const email = document.getElementById("email")?.value.trim() || "";
+        const area = document.getElementById("area")?.value || "";
+        const mensagem = document.getElementById("mensagem")?.value.trim() || "";
+        const cidade = document.getElementById("cidade")?.value.trim() || "";
 
         const texto =
-            "Olá, gostaria de atendimento jurídico.\n\n" +
+            "Olá, gostaria de parceria jurídica.\n\n" +
             "Nome: " + nome + "\n" +
             "E-mail: " + email + "\n" +
-            "Área de Interesse: " + area + "\n\n" +
-            "Mensagem:\n" + mensagem;
+            "Cidade/Estado: " + cidade + "\n" +
+            "Área de Atuação: " + area + "\n\n" +
+            "Experiência:\n" + mensagem;
 
         const link = "https://wa.me/" + NUMERO_WHATSAPP + "?text=" + encodeURIComponent(texto);
 
         window.open(link, "_blank");
 
         form.reset();
-        
     });
-    
 
 });
-
-
