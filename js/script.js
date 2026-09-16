6103c0d3ca8116ca342b38e70cdb934e1acbc234
Nathyane Oliveira <nathyaneoliveira@hotmail.com>
2026-09-16 10:58:22 -0300

    Atualiza landing page Rodrigo

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
        CONFIGURAÇÕES
        ===================================================== */
 
+    /*const NUMERO_CONTATO = "5583998565071";
+    const NUMERO_PARCERIA = "5583998565071";*/
     const NUMERO_CONTATO = "5583988731369";
-    const NUMERO_PARCERIA = "5583998565071";
+    const NUMERO_PARCERIA = "5583988731369";
 
     const FORMULARIOS = {
         contato: {
